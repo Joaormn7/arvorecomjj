d7ba0ade64680c9b105e3ed641a8b6375cc012a2
Joaorm7 <joaogabrielseti311006@gmail.com>
2025-05-22 20:22:17 -0300

    Implementação recursividade

--- src/Arvore.java
+++ src/Arvore.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Arvore {
     private No Raiz;
 
@@ -8,27 +11,61 @@ public class Arvore {
 
     }
 
-    public int conatagemNo() {
-        No aux = Raiz;
-        int contador = 0;
-        while (aux != null) {
-            if (aux.getDireita() != null) {
-                aux = aux.getDireita();
-                contador++;
+    public int conatagemNo(No node) {
+        if(node==null) return 0;
+        return 1 + conatagemNo(node.getEsquerda()) + conatagemNo(node.getDireita());
+    }
+
+    public void percorrerPreOrdem(No node){
+       if(node != null){
+           System.out.println(node.getConteudo()+" ");
+           percorrerPreOrdem(node.getEsquerda());
+           percorrerPreOrdem(node.getDireita());
+       }
+    }
+    public void percorrerEmOrdem(No node) {
+        if (node != null) {
+            percorrerEmOrdem(node.getEsquerda());
+            System.out.println(node.getConteudo() + " ");
+            percorrerEmOrdem(node.getDireita());
+        }
+    }
+        public void percorrerPosOrdem(No node) {
+            if(node != null){
+                percorrerPosOrdem(node.getEsquerda());
+                percorrerPosOrdem(node.getDireita());
+                System.out.println(node.getConteudo()+" ");
             }
         }
 
-        contador++;
-        return contador;
-    }
-    public void percorrerPreOrdem(){
-        No aux = Raiz;
-        if (aux == null){
+    public void buscaEmNivel() {
+        if (Raiz == null) return;
 
+        Queue<No> fila = new LinkedList<>();
+        fila.add(Raiz);
 
+        while (!fila.isEmpty()) {
+            No atual = fila.poll();
+            System.out.print(atual.getConteudo() + " ");
+
+            if (atual.getEsquerda() != null) fila.add(atual.getEsquerda());
+            if (atual.getDireita() != null) fila.add(atual.getDireita());
         }
-
+    }
+    //grande kleytão eterno
+    public int conatagemNoRecursivo() {
+    int contador = 0;
+    No aux = this.Raiz;
+    if (aux.getDireita() != null){
+        contador++;
     }
 
     }
+
+        return 0;
+    }
+
+
+}
+
 
